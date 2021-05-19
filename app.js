4fd6d4a44d6ee0e6be6004fdbef5985008d8a18a
maximilianozurita <maxi.zurita.seb@gmail.com>
2021-05-18 22:06:45 -0300

    Se incluye procfile

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-app.listen(3030,()=>{
-    console.log("Servidor ejecutado");
-});
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+})
 
 //Lo que esta en la carpeta public sera usado.
 const publicPath=path.resolve(__dirname + "/public");
